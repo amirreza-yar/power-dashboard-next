9f8025ce5d517eec415a7e6cbdc3ad1b3e735a6a
amirreza <yar.amirreza@gmail.com>
2023-12-09 03:01:16 +0330

    scripts updates

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,11 @@ export default function RootLayout({ children }) {
   return (
     <html id="html" lang="en" dir="rtl">
       <body>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js" async></script>
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js" async></script> */}
         {/* <script src="https://cdn.jsdelivr.net/npm/apexcharts" defer></script> */}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.44.0/apexcharts.min.js"></script>
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.44.0/apexcharts.min.js"></script> */}
+        <script src="http://rcpss-sutech.ir/js/apexchart.js" defer></script>
+        <script src="http://rcpss-sutech.ir/js/flowbite.min.js" defer></script>
         {children}
       </body>
     </html>
